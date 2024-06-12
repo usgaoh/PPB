ccee83e2ecdbe4a4d5f0947d8bfabefa4b9f8257
unknown <agusputraa9999@gmail.com>
2024-06-12 15:18:02 +0800

    revisi terbaru

--- lib/home.dart
+++ lib/home.dart
@@ -1,45 +1,50 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fomus/form_result.dart'; // Import halaman baru
 
-class Home extends StatefulWidget {
+class Home extends StatefulWidget{
   @override
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home>{
   final _formKey = GlobalKey<FormState>();
 
-  bool nilaiCheckBox1 = false;
-  bool nilaiCheckBox2 = false;
-  bool nilaiSwitch1 = true;
-  bool nilaiSwitch2 = true;
+
+bool nilaiCheckBox1 = false;
+bool nilaiCheckBox2 = false;
+bool nilaiSwitch1 = true;
+bool nilaiSwitch2 = true;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  Widget build(BuildContext context){
+    return new Scaffold(
       backgroundColor: Color.fromARGB(255, 100, 100, 100),
-      appBar: AppBar(
-        leading: Icon(Icons.menu, size: 30, color: Colors.black),
-        title: Text("Fomus", style: TextStyle(color: Colors.black)),
+
+      appBar: new AppBar(
+        leading: new Icon(Icons.menu, size: 30, color: Colors.black),
+        title: new Text("Fomus",
+        style:TextStyle(color: Colors.black)),
         actions: [
           Container(
             width: 40,
             height: 40,
-            margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+            margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
             decoration: BoxDecoration(
-                image: DecorationImage(image: NetworkImage("assets/images/fomus.jpg"))),
+              image: DecorationImage(image: NetworkImage("assets/images/fomus.jpg"))
+            ),
           ),
         ],
         backgroundColor: Colors.white,
-      ),
+        ),
+
       body: ListView(
         key: _formKey,
-        children: [
-          Container(
+          children: [
+            Container(
             width: 500,
             height: 600,
-            margin: EdgeInsets.fromLTRB(50, 100, 50, 100),
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            margin: const EdgeInsets.fromLTRB(50, 100, 50, 100),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
             decoration: BoxDecoration(
               boxShadow: [
                 BoxShadow(
@@ -50,14 +55,18 @@ class _HomeState extends State<Home> {
               color: Colors.white,
               borderRadius: BorderRadius.circular(15),
             ),
+
             child: Column(
               children: <Widget>[
+
                 TextField(
-                  decoration: InputDecoration(
+                  decoration: new InputDecoration(
                     hintText: "Formulir tanpa judul",
                     labelText: "Judul formulir",
                   ),
                 ),
+          
+
                 Padding(
                   padding: const EdgeInsets.only(top: 50.0),
                   child: TextFormField(
@@ -76,6 +85,7 @@ class _HomeState extends State<Home> {
                     },
                   ),
                 ),
+
                 SwitchListTile(
                   title: Text('Wajib diisi'),
                   value: nilaiSwitch1,
@@ -87,6 +97,7 @@ class _HomeState extends State<Home> {
                     });
                   },
                 ),
+
                 Padding(
                   padding: const EdgeInsets.only(top: 50.0),
                   child: TextFormField(
@@ -105,6 +116,7 @@ class _HomeState extends State<Home> {
                     },
                   ),
                 ),
+
                 SwitchListTile(
                   title: Text('Wajib diisi'),
                   value: nilaiSwitch2,
@@ -116,7 +128,8 @@ class _HomeState extends State<Home> {
                     });
                   },
                 ),
-                Padding(padding: EdgeInsets.only(top: 20.0)),
+
+                new Padding(padding: new EdgeInsets.only(top: 20.0),),
                 CheckboxListTile(
                   title: Text('Opsi 1'),
                   value: nilaiCheckBox1,
@@ -127,6 +140,7 @@ class _HomeState extends State<Home> {
                     });
                   },
                 ),
+
                 CheckboxListTile(
                   title: Text('Opsi 2'),
                   value: nilaiCheckBox2,
@@ -137,6 +151,7 @@ class _HomeState extends State<Home> {
                     });
                   },
                 ),
+               
                 ElevatedButton(
                   child: Text(
                     "Submit",
@@ -147,15 +162,7 @@ class _HomeState extends State<Home> {
                   ),
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
-                      // Navigasi ke halaman baru dengan hasil formulir
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => FormResultPage(
-                            formTitle: "Hasil Formulir",
-                          ),
-                        ),
-                      );
+                      // Perform some action
                     }
                   },
                 ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fomus/home.dart';
 
+
 void main() {
   runApp( MyApp());
 }
